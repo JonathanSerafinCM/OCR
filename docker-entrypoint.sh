0d5af39e9788252e79b17b12a699edcf77ef714c
Jonathan Serafin Contreras Malfavon <167220457+JonathanSerafinCM@users.noreply.github.com>
2024-11-04 00:35:44 -0600

    Enhance Docker entrypoint and Nginx configuration: clear and cache routes, add API handling, and improve PHP-FPM settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,13 @@ mkdir -p /run/php /var/log
 touch /var/log/php-fpm-error.log
 chown -R www-data:www-data /run/php /var/log/php-fpm-error.log
 
+# Laravel setup
 php artisan key:generate
 php artisan migrate --force
 php artisan cache:clear
 php artisan config:clear
+php artisan route:clear   # Clear routes first
+php artisan route:cache   # Then cache routes
 
-php-fpm -D
-nginx -g 'daemon off;'
+php-fpm -D  # Start php-fpm in the background
+nginx -g 'daemon off;'  # Run nginx in the foreground
