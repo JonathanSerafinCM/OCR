ae076eec1b52cb4d51ae9c94a8cfdbfb150777fd
Jonathan Serafin Contreras Malfavon <167220457+JonathanSerafinCM@users.noreply.github.com>
2024-11-20 16:02:47 -0600

    Add API authentication support and reorganize routes for menu processing and user preferences

--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,28 @@ use App\Http\Controllers\MenuOCRController;
 // Public routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/menu/process', [MenuOCRController::class, 'processMenu']);
+Route::get('/menu/items', [MenuOCRController::class, 'getMenuItems']);
+Route::post('/dish/view', [MenuOCRController::class, 'recordDishView']);
+Route::get('/menu/popular', [MenuOCRController::class, 'getPopularDishes']);
+Route::get('/preferences', [MenuOCRController::class, 'showPreferences']);
+Route::get('/preferences/api', [MenuOCRController::class, 'getUserPreferencesApi']);
+Route::post('/preferences', [MenuOCRController::class, 'updatePreferences']);
+Route::get('/menu/recommendations', [MenuOCRController::class, 'getRecommendations']);
+
+// API routes without authentication
+Route::middleware([])->group(function () {
+    // User preferences and recommendations
+    Route::post('/preferences/update', [MenuOCRController::class, 'updatePreferences']);
+    
+    // Tracking and analytics
+});
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     // Menu processing routes
-    Route::post('/menu/process', [MenuOCRController::class, 'processMenu']);
-    Route::get('/menu/items', [MenuOCRController::class, 'getMenuItems']);
-    
-    // User preferences and recommendations
-    Route::post('/preferences/update', [MenuOCRController::class, 'updatePreferences']);
-    Route::get('/preferences', [MenuOCRController::class, 'showPreferences']);
-    Route::get('/preferences/api', [MenuOCRController::class, 'getUserPreferencesApi']);
-    Route::post('/preferences', [MenuOCRController::class, 'updatePreferences']);
-    Route::get('/menu/recommendations', [MenuOCRController::class, 'getRecommendations']);
     
     // Tracking and analytics
-    Route::post('/dish/view', [MenuOCRController::class, 'recordDishView']);
-    Route::get('/menu/popular', [MenuOCRController::class, 'getPopularDishes']);
     
     // Filtering
     Route::post('/menu/filter', [MenuOCRController::class, 'filterMenuItems']);
